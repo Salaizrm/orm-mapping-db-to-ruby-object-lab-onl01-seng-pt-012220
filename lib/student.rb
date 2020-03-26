b2a9e8425ca5caaca0d40c0119f9387ee7d17818
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 01:40:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,14 +48,12 @@ class Student
   end
 
   def self.students_below_12th_grade
+  sql = <<-SQL
+     SELECT COUNT(grade > 10) FROM students
+  SQL
+  DB[:conn].execute(sql)
+end
 
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade < 12
-    SQL
-    DB[:conn].execute(sql)
-  end
 
   def save
     sql = <<-SQL
