6bcf7857108645d7b19ef6debf6cf7e8695ad0c5
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 01:55:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
