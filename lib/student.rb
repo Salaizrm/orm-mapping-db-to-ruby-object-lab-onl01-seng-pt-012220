63b197a18adab3bb3a2f8ff9ed83b084283e0685
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 01:50:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,8 +65,11 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-
     SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
