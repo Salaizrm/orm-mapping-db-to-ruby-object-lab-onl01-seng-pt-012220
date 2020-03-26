859e9a4767d44c7a757ccd33ca49682f41afc306
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 01:54:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
 
     sql = <<-SQL
       SELECT *
@@ -68,7 +68,7 @@ class Student
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
