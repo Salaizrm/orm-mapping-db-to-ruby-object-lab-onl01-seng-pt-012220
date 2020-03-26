6add99ec6feb8c4e02d54d222662a2b670b10a84
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-26 01:53:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,15 +59,14 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE grade = 10
-      LIMIT DESC 1
+    SELECT *
+    FROM students
+    WHERE students.grade = 10
+    LIMIT 1
     SQL
-
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end.first
   end
